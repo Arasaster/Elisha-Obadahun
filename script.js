a549ab1825f7210df07ef80bb3247a9eef3f19c9
Elisha Obadahun <90161239+Arasaster@users.noreply.github.com>
2025-09-11 19:33:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -50,3 +50,17 @@ window.ontouchend = e => handleOnUp(e.touches[0]);
 window.onmousemove = e => handleOnMove(e);
 window.ontouchmove = e => handleOnMove(e.touches[0]);
 
+
+/* Modal */
+const open = document.getElementById("open")
+const modal_container = document.getElementById("modal-container")
+const close = document.getElementById("close")
+
+open.addEventListener('click', () => {
+  modal_container.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+  modal_container.classList.remove('show');
+});
+
